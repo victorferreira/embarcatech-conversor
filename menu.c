7ce80bd5ea66ad2ff4a46cecc6627639882e32d7
Ueslei Jesus <uesleijesusdev@gmail.com>
2024-12-26 11:32:14 -0300

    Função que exibe o e gerencia o menu principal

--- menu.c
+++ menu.c
@@ -33,3 +33,60 @@ int main() {
     menu();
     return 0;
 }
+
+// Função que exibe o e gerencia o menu principal
+
+void menu() {
+    int opcao;
+    
+    do {
+        printf("\n=== Conversor de Unidades ===\n");
+        printf("1. Conversor de Comprimento\n");
+        printf("2. Conversor de Massa\n");
+        printf("3. Conversor de Volume\n");
+        printf("4. Conversor de Temperatura\n");
+        printf("5. Conversor de Velocidade\n");
+        printf("6. Conversor de Potência\n");
+        printf("7. Conversor de Área\n");
+        printf("8. Conversor de Tempo\n");
+        printf("9. Conversor de Dados\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        
+        switch(opcao) {
+            case 1:
+                conversor_comprimento();
+                break;
+            case 2:
+                conversor_massa();
+                break;
+            case 3:
+                conversor_volume();
+                break;
+            case 4:
+                conversor_temperatura();
+                break;
+            case 5:
+                conversor_velocidade();
+                break;
+            case 6:
+                conversor_potencia();
+                break;
+            case 7:
+                conversor_area();
+                break;
+            case 8:
+                conversor_tempo();
+                break;
+            case 9:
+                conversor_dados();
+                break;
+            case 0:
+                printf("Programa encerrado!\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while(opcao != 0);
+}
