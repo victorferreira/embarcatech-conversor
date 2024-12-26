448036d2998b81a0ab8875c755788b46c54c0d8a
Ueslei Jesus <uesleijesusdev@gmail.com>
2024-12-26 11:28:04 -0300

    arquivo de menu do programa e protótipo das funções de conversão

---
+++ menu.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Protótipos das funções de conversão
+void conversor_comprimento();
+void conversor_massa();
+void conversor_volume();
+void conversor_temperatura();
+void conversor_velocidade();
+void conversor_potencia();
+void conversor_area();
+void conversor_tempo();
+void conversor_dados();
+void menu();
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Protótipos das funções de conversão
+void conversor_comprimento();
+void conversor_massa();
+void conversor_volume();
+void conversor_temperatura();
+void conversor_velocidade();
+void conversor_potencia();
+void conversor_area();
+void conversor_tempo();
+void conversor_dados();
+void menu();
+
+
